e133ad11a19aaba7afbebc079804fd94bf08bdf1
tiriltorseth <tiril.torseth@gmail.com>
2026-03-07 23:53:23 +0100

    Suite.cs

--- Arbeidskrav2/Suite.cs
+++ Arbeidskrav2/Suite.cs
@@ -1,6 +1,30 @@
 namespace Arbeidskrav2;
 
-public class Suite
+public class Suite : Room
 {
+    private bool hasJacuzzi;
+    private bool hasLounge;
+
+    public bool HasJacuzzi
+    {
+        get { return hasJacuzzi; }
+        protected set { hasJacuzzi = value; }
+    }
     
+    public bool HasLounge{
+        get { return hasLounge; }
+        protected set { hasLounge = value; }
+    }
+    
+    public Suite(string RoomType, decimal PricePerNight, int MaxGuests)
+        : base(RoomType, 3500, 4)
+    {
+        HasJacuzzi = true;
+        HasLounge = true;
+    }
+
+    public override void DisplayRoomInfo()
+    {
+        /// Her skriver du ut informasjonen fra menyen, kall på denne metoden
+    }
 }
