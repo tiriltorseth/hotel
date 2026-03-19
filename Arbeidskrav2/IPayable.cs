af0fe1b14bd7fe93bf4353008fb8a35702ab4af3
tiriltorseth <tiril.torseth@gmail.com>
2026-03-19 20:47:18 +0100

    ipayable kommentert

--- Arbeidskrav2/IPayable.cs
+++ Arbeidskrav2/IPayable.cs
@@ -1,10 +1,19 @@
 namespace Arbeidskrav2;
 
+/// <summary>
+/// Interface for simulering av betaling
+/// </summary>
 public interface IPayable
 {
 
+    /// <summary>
+    /// Bool som tar inn amount og viser betalingsprosess
+    /// </summary>
     public bool ProcessPayment(decimal amount);
 
+    /// <summary>
+    /// Metode som som henter betalingsinformasjon i en string
+    /// </summary>
     public string GetPaymentInfo();
 }
 
